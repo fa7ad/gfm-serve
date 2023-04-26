f80407272ddad92fe52d82eec035745149ec2ded
Fahad Hossain <set.name.fahad@gmail.com>
2023-04-27 01:10:14 +0200

    feat: add css

--- go.mod
+++ go.mod
@@ -2,14 +2,18 @@ module github.com/fa7ad/gfm-serve
 
 go 1.20
 
-require github.com/urfave/cli/v2 v2.25.1
+require (
+	github.com/antchfx/htmlquery v1.3.0
+	github.com/gofiber/fiber/v2 v2.44.0
+	github.com/urfave/cli/v2 v2.25.1
+	golang.org/x/net v0.9.0
+)
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/xpath v1.2.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/gofiber/fiber/v2 v2.44.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -25,6 +29,6 @@ require (
 	github.com/valyala/fasthttp v1.46.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- internal/gfm_server/serve.go
+++ internal/gfm_server/serve.go
@@ -1,14 +1,21 @@
 package gfm_server
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"path/filepath"
-	"regexp"
+	"strings"
 
+	"github.com/antchfx/htmlquery"
 	"github.com/gofiber/fiber/v2"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/net/html"
 )
+
+const Css = `
+body { margin: 0; padding: 30px; background: #fff; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; -webkit-font-smoothing: antialiased;}main { max-width: 920px;}header { display: flex; justify-content: space-between; flex-wrap: wrap;}h1 { font-size: 18px; font-weight: 500; margin-top: 0; color: #000;}header h1 a { font-size: 18px; font-weight: 500; margin-top: 0; color: #000;}h1 i { font-style: normal;}ul { margin: 0 0 0 -2px; padding: 20px 0 0 0;}ul li { list-style: none; font-size: 14px; display: flex; justify-content: space-between;}a { text-decoration: none;}ul a { color: #000; padding: 10px 5px; margin: 0 -5px; white-space: nowrap; overflow: hidden; display: block; width: 100%; text-overflow: ellipsis;}header a { color: #0076FF; font-size: 11px; font-weight: 400; display: inline-block; line-height: 20px;}svg { height: 13px; vertical-align: text-bottom;}ul a::before { display: inline-block; vertical-align: middle; margin-right: 10px; width: 24px; text-align: center; line-height: 12px;}ul a.file::before { content: url("data:image/svg+xml;utf8,<svg width='15' height='19' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M10 8C8.34 8 7 6.66 7 5V1H3c-1.1 0-2 .9-2 2v13c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8h-4zM8 5c0 1.1.9 2 2 2h3.59L8 1.41V5zM3 0h5l7 7v9c0 1.66-1.34 3-3 3H3c-1.66 0-3-1.34-3-3V3c0-1.66 1.34-3 3-3z' fill='black'/></svg>");}ul a:hover { text-decoration: underline;}ul a.dir::before { content: url("data:image/svg+xml;utf8,<svg width='20' height='16' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M18.784 3.87a1.565 1.565 0 0 0-.565-.356V2.426c0-.648-.523-1.171-1.15-1.171H8.996L7.908.25A.89.89 0 0 0 7.302 0H2.094C1.445 0 .944.523.944 1.171v2.3c-.21.085-.398.21-.565.356a1.348 1.348 0 0 0-.377 1.004l.398 9.83C.42 15.393 1.048 16 1.8 16h15.583c.753 0 1.36-.586 1.4-1.339l.398-9.83c.021-.313-.125-.69-.397-.962zM1.843 3.41V1.191c0-.146.104-.272.25-.272H7.26l1.234 1.088c.083.042.167.104.293.104h8.282c.125 0 .25.126.25.272V3.41H1.844zm15.54 11.712H1.78a.47.47 0 0 1-.481-.46l-.397-9.83c0-.147.041-.252.125-.356a.504.504 0 0 1 .377-.147H17.78c.125 0 .272.063.377.147.083.083.125.209.125.334l-.418 9.83c-.021.272-.23.482-.481.482z' fill='black'/></svg>");}ul a.lambda::before { content: url("data:image/svg+xml; utf8,<svg width='15' height='19' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M3.5 14.4354H5.31622L7.30541 9.81311H7.43514L8.65315 13.0797C9.05676 14.1643 9.55405 14.5 10.7 14.5C11.0171 14.5 11.291 14.4677 11.5 14.4032V13.1572C11.3847 13.1766 11.2622 13.2024 11.1541 13.2024C10.6351 13.2024 10.3829 13.0281 10.1595 12.4664L8.02613 7.07586C7.21171 5.01646 6.54865 4.5 5.11441 4.5C4.83333 4.5 4.62432 4.53228 4.37207 4.59038V5.83635C4.56667 5.81052 4.66036 5.79761 4.77568 5.79761C5.64775 5.79761 5.9 6.0042 6.4045 7.19852L6.64234 7.77954L3.5 14.4354Z' fill='black'/><rect x='0.5' y='0.5' width='14' height='18' rx='2.5' stroke='black'/></svg>");}ul a.file.gif::before,ul a.file.jpg::before,ul a.file.png::before,ul a.file.svg::before { content: url("data:image/svg+xml;utf8,<svg width='16' height='16' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg' fill='none' stroke='black' stroke-width='5' stroke-linecap='round' stroke-linejoin='round'><rect x='6' y='6' width='68' height='68' rx='5' ry='5'/><circle cx='24' cy='24' r='8'/><path d='M73 49L59 34 37 52m16 20L27 42 7 58'/></svg>");}::selection { background-color: #79FFE1; color: #000;}::-moz-selection { background-color: #79FFE1; color: #000;}@media (min-width: 768px) { ul {display: flex;flex-wrap: wrap; } ul li {width: 230px;padding-right: 20px; }}@media (min-width: 992px) { body {padding: 45px; } h1, header h1 a {font-size: 15px; } ul li {font-size: 13px;box-sizing: border-box;justify-content: flex-start; }}
+`
 
 func ServeGfm(c *cli.Context) error {
 	port := c.Int("port")
@@ -28,7 +35,7 @@ func ServeGfm(c *cli.Context) error {
 
 	app.Static("/", directory, fiber.Static{
 		Browse:         true,
-		ModifyResponse: decideMdOrFile,
+		ModifyResponse: serveMiddleware,
 	})
 
 	fmt.Printf("Serving files from `%s` on http://%s:%d\n", full_path, host, port)
@@ -37,13 +44,99 @@ func ServeGfm(c *cli.Context) error {
 	return nil
 }
 
-func decideMdOrFile(c *fiber.Ctx) error {
-	body := c.Response().Body()
-	newbody := string(body)
-	mtime := regexp.MustCompile(`, (?:file|dir),.*?last modified.*?\<`)
+func serveMiddleware(c *fiber.Ctx) error {
+	_body := c.Response().Body()
+	body := make([]byte, len(_body))
+	copy(body, _body)
 
-	newbody = mtime.ReplaceAllString(newbody, `<`)
+	c.Response().ResetBody()
 
-	c.Send([]byte(newbody))
+	document, err := htmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	styleTag := &html.Node{
+		Type: html.ElementNode,
+		Data: "style",
+	}
+	styleTag.AppendChild(&html.Node{
+		Type: html.TextNode,
+		Data: Css,
+	})
+
+	h1, err := htmlquery.Query(document, "//h1")
+	if err != nil || h1 == nil {
+		return err
+	}
+
+	h1.Parent.AppendChild(styleTag)
+
+	header := &html.Node{
+		Type: html.ElementNode,
+		Data: "header",
+	}
+	h1.Parent.AppendChild(header)
+	h1.Parent.RemoveChild(h1)
+	header.AppendChild(h1)
+
+	h1txt := h1.LastChild.Data
+	h1.LastChild.Data = ""
+	itag := &html.Node{
+		Type: html.ElementNode,
+		Data: "i",
+	}
+	itag.AppendChild(&html.Node{
+		Type: html.TextNode,
+		Data: "Index of ",
+	})
+	anchor := &html.Node{
+		Type: html.ElementNode,
+		Data: "a",
+		Attr: []html.Attribute{
+			{
+				Key: "href",
+				Val: "#",
+			},
+		},
+	}
+	anchor.AppendChild(&html.Node{
+		Type: html.TextNode,
+		Data: h1txt,
+	})
+	h1.AppendChild(itag)
+	h1.AppendChild(anchor)
+
+	ul, err := htmlquery.Query(document, "//ul")
+	if err != nil || ul == nil {
+		return err
+	}
+
+	main := &html.Node{
+		Type: html.ElementNode,
+		Data: "main",
+	}
+
+	ul.Parent.AppendChild(main)
+	ul.Parent.RemoveChild(ul)
+	main.AppendChild(ul)
+
+	listItems, err := htmlquery.QueryAll(document, "//li")
+	if err != nil {
+		return err
+	}
+
+	for _, listItem := range listItems {
+		if strings.HasPrefix(listItem.FirstChild.LastChild.Data, ".") {
+			parent := listItem.Parent
+			parent.RemoveChild(listItem)
+			continue
+		}
+
+		listItem.LastChild.Data = ""
+	}
+
+	output := htmlquery.OutputHTML(document, true)
+	c.Response().SetBody([]byte(output))
 	return nil
 }
